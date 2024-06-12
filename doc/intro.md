4d530018efba1b0725e9ff7fdc9cdb673821dc74
Fredrik Vaeng Røtnes <fredrik.roetnes@ardoq.com>
2024-06-12 12:40:53 +0200

    hot intern summer init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to interns-tutorial
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/interns_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns interns-tutorial.core-test
+  (:require [clojure.test :refer :all]
+            [interns-tutorial.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
