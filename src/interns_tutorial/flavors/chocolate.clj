92b3da785ee8ce2443839947af658f6e5722252d
Fredrik Vaeng Røtnes <fredrik.roetnes@ardoq.com>
2024-06-14 10:19:20 +0200

    Ice cream store init

---
+++ src/interns_tutorial/flavors/chocolate.clj
@@ -0,0 +1,5 @@
+(ns interns-tutorial.flavors.chocolate)
+;; dir1.dir2.dir3.filename
+
+;; the ns ardoq.api.component-api is in
+;; ardoq/api/component_api.clj
---
+++ src/interns_tutorial/ice_cream_store.clj
@@ -0,0 +1,7 @@
+(ns interns-tutorial.ice-cream-store)
+
+(defn make-ice-cream [recipe ingredients]
+  )
+
+(defn sell-ice-cream [stock ice-cream-flavour]
+  )
