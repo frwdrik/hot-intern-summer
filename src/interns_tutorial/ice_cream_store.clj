5d51a621ce9c9db15638c8a02efebb4afb36c619
Fredrik Vaeng Røtnes <fredrik.roetnes@ardoq.com>
2024-06-14 10:38:21 +0200

    making progress, destructuring

--- src/interns_tutorial/ice_cream_store.clj
+++ src/interns_tutorial/ice_cream_store.clj
@@ -1,7 +1,49 @@
 (ns interns-tutorial.ice-cream-store)
 
-(defn make-ice-cream [recipe ingredients]
-  )
+(def recipes
+  ;; every quantity is in grams
+  {:pistaccio [{:name :pistaccio-nuts
+                :quantity 20}
+               {:name :milk
+                :quantity 100}
+               {:name :sugar
+                :quantity 50}]})
 
-(defn sell-ice-cream [stock ice-cream-flavour]
+(def ingredients-in-stock
+  {:pistaccio-nuts 100
+   :milk 1000
+   :sugar 500})
+
+(defn have-sufficient-quantity-in-stock? [ingredient-with-name-and-quantity]
+  (let [ingredient-name (:name ingredient-with-name-and-quantity)
+        ingredient-quantity (:quantity ingredient-with-name-and-quantity)]
+    (>= (get ingredients-in-stock ingredient-name)
+        ingredient-quantity)))
+
+(defn have-sufficient-quantity-in-stock?-v2
+  [{:keys [name quantity]}]
+  (>= (get ingredients-in-stock name)
+      quantity))
+
+(comment
+  ;; what is destructuring?
+ (defn foo [{:keys [bar]}]
+   bar)
+
+ (foo {})
+ (foo {:bar 123123})
+ )
+
+(defn make-ice-cream [flavor ingredients]
+  (let [recipe (get recipes flavor)]
+    ;; [{:name :pistaccio-nuts, :quantity 20}
+    ;;  {:name :milk, :quantity 100}
+    ;;  {:name :sugar, :quantity 50}]
+    (every? have-sufficient-quantity-in-stock? recipe)))
+
+(comment
+ (make-ice-cream :pistaccio nil)
+ )
+
+(defn sell-ice-cream [stock flavor]
   )
